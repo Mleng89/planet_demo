57cf5fe60517d64fac816a0ebb1d2124cea6e227
Matthew Leng <mleng89@gmail.com>
2022-02-23 18:22:42 -0500

    Added: planet velocity and update position func call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import math
 from planet import Planet
 
 pygame.init()
@@ -25,21 +24,30 @@ def main():
     sun.sun = True
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_velocity = 29.783 * 1000
+
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_velocity = 24.077 * 1000
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**24)
+    mercury.y_velocity = -47.4 * 1000
+
     venus = Planet(0.723 * Planet.AU, 0, 14, ODDLACE, 4.8685 * 10**24)
+    venus.y_velocity = -35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
+
     while run:
         clock.tick(60)
-        # WIN.fill(WHITE)
+        WIN.fill((0, 0, 0))
 
         for event in pygame.event.get():
             # print("what are my events:", event)
             if event.type == pygame.QUIT:
                 run = False
+
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
         # Updates to draw planets at the moment
         pygame.display.update()
